f9da636df41aa026708c0fb0086c3eacf8f90b28
ALEXANDRE DIAS BRIGIDO <alexbrigido@hotmail.com>
2021-05-31 21:08:02 -0300

    added icones

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -9,6 +9,7 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <WordPair>{};
 
   @override
   Widget build(BuildContext context) {
@@ -24,13 +25,10 @@ class _RandomWordsState extends State<RandomWords> {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
         itemBuilder: (BuildContext _context, int i) {
-
           if (i.isOdd) {
             return Divider();
           }
-
           final int index = i ~/ 2;
-
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
@@ -40,10 +38,15 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
