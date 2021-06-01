e5f73bee4b6db78aacc4badead7818923b2af0ca
ALEXANDRE DIAS BRIGIDO <alexbrigido@hotmail.com>
2021-05-31 21:19:15 -0300

    navegacao

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -16,6 +16,9 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold (
       appBar: AppBar(
         title: Text('Começando com Gerador de Nomes'),
+        actions: [
+          IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -59,4 +62,35 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
 }
